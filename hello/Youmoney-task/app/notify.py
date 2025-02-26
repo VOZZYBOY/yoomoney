459e778c94d06c634dd6b43b900c8d8f3f0a7e67
Эрик <92530772+VOZZYBOY@users.noreply.github.com>
2025-02-26 07:29:05 +0300

    Update notify.py

--- hello/Youmoney-task/app/notify.py
+++ hello/Youmoney-task/app/notify.py
@@ -1,6 +1,10 @@
+
 import requests
 import logging
-from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
@@ -11,9 +15,9 @@ def send_notification(message: str):
     Отправляет уведомление в Телеграм.
     """
     log.debug(f"Отправляю сообщение в ТГ бота: {message}")
-    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
+    url = f"https://api.telegram.org/bot{os.environ.get('TELEGRAM_BOT_TOKEN')}/sendMessage"
     data = {
-        'chat_id': TELEGRAM_CHAT_ID,
+        'chat_id': os.environ.get('TELEGRAM_CHAT_ID'),
         'text': message
     }
     requests.post(url, data=data)
