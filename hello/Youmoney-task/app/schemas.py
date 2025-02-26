c1a890e13b4f2cb0e14c7f8d20d50dea6a9fcd50
Эрик <92530772+VOZZYBOY@users.noreply.github.com>
2025-02-26 07:29:25 +0300

    Update schemas.py

--- hello/Youmoney-task/app/schemas.py
+++ hello/Youmoney-task/app/schemas.py
@@ -1,23 +1,25 @@
-from marshmallow import (Schema,
-                         fields,
-                         validates,
-                         ValidationError,
-                         post_load)
 
+from marshmallow import Schema, fields, validates, ValidationError, post_load
 
 class PaymentSchema(Schema):
     amount = fields.Dict(required=True)
     user_id = fields.Str(required=True)
     order_id = fields.Int(required=True)
+    is_recurrent = fields.Boolean(required=False) 
 
     @validates('amount')
     def validate_amount(self, amount: dict) -> None:
         if 'value' not in amount or 'currency' not in amount:
-            raise ValidationError('Словарь amount должен содержать'
-                                  'поля "value" и "currency".')
-        if not isinstance(amount['value'], (str)) or \
+            raise ValidationError('Словарь amount должен содержать поля "value" и "currency".')
+        if not isinstance(amount['value'], (str, int, float)) or \
            not isinstance(amount['currency'], str):
-            raise ValidationError('value и currency должны быть строками.')
+            raise ValidationError('value должно быть числом, currency - строкой.')
+        
+        try:
+            float(amount['value'])
+        except ValueError:
+            raise ValidationError('value должно быть числом.')
+
 
     @post_load
     def make_payment(self, data: dict, **kwargs) -> dict:
@@ -26,17 +28,20 @@ class PaymentSchema(Schema):
 
 class RefundSchema(Schema):
     amount = fields.Dict(required=True)
-    user_id = fields.Str(required=True)
     payment_id = fields.Str(required=True)
 
     @validates('amount')
     def validate_amount(self, amount: dict) -> None:
         if 'value' not in amount or 'currency' not in amount:
-            raise ValidationError('Словарь amount должен содержать'
-                                  'поля "value" и "currency".')
-        if not isinstance(amount['value'], (str)) or \
+            raise ValidationError('Словарь amount должен содержать поля "value" и "currency".')
+        if not isinstance(amount['value'], (str, int, float)) or \
            not isinstance(amount['currency'], str):
-            raise ValidationError('value и currency должны быть строками.')
+            raise ValidationError('value должно быть числом, currency - строкой.')
+        
+        try:
+            float(amount['value'])
+        except ValueError:
+            raise ValidationError('value должно быть числом.')
 
     @post_load
     def make_payment(self, data: dict, **kwargs) -> dict:
